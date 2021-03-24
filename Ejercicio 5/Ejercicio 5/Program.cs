4c513793274bd52d5592c32a0b06e977c8f25ac0
demialko <demialko@gmail.com>
2021-03-24 13:57:22 -0300

    Solución de ejercicio 5.
    Solución para ejercicios 5 y 6. La solución del ejercicio 6 es mejorable.

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Describe la tecla presioanda de la consola
+            ConsoleKeyInfo input;
+      
+            do
+            {
+                Console.WriteLine("Ingrese CTRL+K, de lo contrario el proceso continuará");
+                //Lee las teclas presionadas. Con false muestra lo presionado en la pantalla, con true no.
+                input = Console.ReadKey(true);
+                //Console.WriteLine(ConsoleModifiers.Control); Esto imprime control, solo para pruebas.
+                //Console.WriteLine(input.Modifiers); Esto imprime el modificador que se use, solo para pruebas.
+                //Console.WriteLine(input.Key); Esto imprime la tecla que se aprete, solo para pruebas.
+            } while (input.Key.ToString() != "K" || input.Modifiers != ConsoleModifiers.Control);
+
+            Console.WriteLine("Ahora agreguemos un Shift!");
+
+            do
+            {
+                Console.WriteLine("Ingrese CTRL+Shift+K, de lo contrario el proceso continuará");
+                input = Console.ReadKey(true);
+                //Console.WriteLine(input.Modifiers); Solo para saber que modificadores se usan
+            } while (input.Key.ToString() != "K" || (input.Modifiers.ToString() != "Shift, Control")); //Medio feo, porque le estoy cargando a mano el resultado Shift, Control.
+
+            Console.WriteLine("El proceso ha finalizado exitosamente");
+            Console.ReadKey();
+        }
+    }
+}
